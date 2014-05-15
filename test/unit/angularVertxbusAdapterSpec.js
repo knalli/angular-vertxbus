de0e1345687fa21a94cc40e7b2fef783b312a4b2
knalli <knallisworld@googlemail.com>
2014-05-16 00:29:38 +0200

    feat(vertxEventBusService): add opt-in feature buffering messages
    if the internal event bus is not connected, the option `messageBuffer`
    defines the number of send/publish emit events which will be buffered
    until the connection is established
    
    relates #7

--- test/unit/angularVertxbusAdapterSpec.js
+++ test/unit/angularVertxbusAdapterSpec.js
@@ -137,4 +137,138 @@ describe('knalli.angular-vertxbus', function () {
 
   });
 
+
+  describe('vertxEventBusService', function () {
+
+    describe(' with disabled message queue (default)', function () {
+      var vertxEventBus, vertxEventBusService, result;
+
+      beforeEach(module('knalli.angular-vertxbus', function (vertxEventBusProvider) {
+        vertxEventBusProvider.useMessageBuffer(0);
+      }));
+
+      beforeEach(inject(function (_vertxEventBus_, _vertxEventBusService_) {
+        vertxEventBus = _vertxEventBus_;
+        vertxEventBusService = _vertxEventBusService_;
+        // Mock bus is closed
+        _vertxEventBus_.readyState = function () {
+          return _vertxEventBus_.EventBus.CLOSED;
+        };
+        var sendCalls = 0;
+        _vertxEventBus_.send = function (address, message, replyHandler) {
+          ++sendCalls;
+          result = {
+            reply: message
+          };
+          if (replyHandler) {
+            replyHandler(result);
+          }
+        };
+        // extend object
+        vertxEventBus.getSendCalls = function () {
+          return sendCalls;
+        };
+      }));
+
+      describe('should not dispatch send', function () {
+        it('when eventbus is closed', function (done) {
+          setTimeout(function () {
+            vertxEventBusService.send('xyz', {data: 1});
+            setTimeout(function () {
+              expect(result).to.be(undefined);
+              expect(vertxEventBusService.getBufferCount()).to.be(0);
+              expect(vertxEventBus.getSendCalls()).to.be(0);
+              done();
+            }, 1000);
+          }, 200);
+        });
+      });
+    });
+
+    describe(' with enabled message queue (size 3)', function () {
+      var vertxEventBus, vertxEventBusService, result;
+
+      beforeEach(module('knalli.angular-vertxbus', function (vertxEventBusProvider) {
+        vertxEventBusProvider.useMessageBuffer(3);
+      }));
+
+      beforeEach(inject(function (_vertxEventBus_, _vertxEventBusService_) {
+        vertxEventBus = _vertxEventBus_;
+        vertxEventBusService = _vertxEventBusService_;
+        // Mock bus is closed
+        vertxEventBus.readyState = function () {
+          return vertxEventBus.EventBus.CLOSED;
+        };
+        var sendCalls = 0;
+        vertxEventBus.send = function (address, message, replyHandler) {
+          ++sendCalls;
+          result = {
+            reply: message
+          };
+          if (replyHandler) {
+            replyHandler(result);
+          }
+        };
+        // extend object
+        vertxEventBus.getSendCalls = function () {
+          return sendCalls;
+        };
+      }));
+
+      describe('when eventbus is closed', function () {
+        it('should dispatch send as queued', function (done) {
+          setTimeout(function () {
+            vertxEventBusService.send('xyz', {data: 123});
+            setTimeout(function () {
+              expect(result).to.be(undefined);
+              expect(vertxEventBusService.getBufferCount()).to.be(1);
+              expect(vertxEventBus.getSendCalls()).to.be(0);
+              done();
+            }, 1000);
+          }, 200);
+        });
+
+        it('should queue max 3 items', function (done) {
+          setTimeout(function () {
+            vertxEventBusService.send('xyz', {data: 1});
+            vertxEventBusService.send('xyz', {data: 2});
+            vertxEventBusService.send('xyz', {data: 3});
+            vertxEventBusService.send('xyz', {data: 4});
+            setTimeout(function () {
+              expect(result).to.be(undefined);
+              expect(vertxEventBusService.getBufferCount()).to.be(3);
+              expect(vertxEventBus.getSendCalls()).to.be(0);
+              done();
+            }, 1000);
+          }, 200);
+        });
+      });
+
+      describe('should replay queued items', function () {
+        it('when eventbus is reopened', function (done) {
+          setTimeout(function () {
+            vertxEventBusService.send('xyz', {data: 1});
+            vertxEventBusService.send('xyz', {data: 2});
+            vertxEventBusService.send('xyz', {data: 3});
+
+            // fake connect
+            vertxEventBus.readyState = function () {
+              return vertxEventBus.EventBus.OPEN;
+            };
+            vertxEventBus.onopen();
+
+            setTimeout(function () {
+              expect(result).to.eql({reply: {data: 3}});
+              expect(vertxEventBusService.getBufferCount()).to.be(0);
+              expect(vertxEventBus.getSendCalls()).to.be(3);
+              done();
+            }, 1000);
+          }, 200);
+        });
+      });
+    });
+
+  });
+
+
 });
