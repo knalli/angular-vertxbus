63262f6e647c85d5f2c3f42fb61217dddaf19346
knalli <knallisworld@googlemail.com>
2015-04-12 20:01:38 +0200

    Update test scope for Angular JS 1.4 using latest RC0

--- test/e2e/web/app.js
+++ test/e2e/web/app.js
@@ -8,7 +8,7 @@
     .useDebug(true)
     .useUrlServer('http://localhost:8080');
   })
-  .run(function ($rootScope) {
+  .run(function ($rootScope, vertxEventBus, vertxEventBusService, $interval) {
     $rootScope.sessionIsValid = false;
     $rootScope.$on('vertx-eventbus.system.login.succeeded', function (event, data) {
       console.log('Vert.X Login succeeded (status)', data);
@@ -18,14 +18,36 @@
       console.log('Vert.X Login failed (status)', data);
       $rootScope.sessionIsValid = false;
     });
+
+    $rootScope.moduleStats = {
+      wrapper: {},
+      service: {}
+    };
+    $interval(function () {
+      try {
+        $rootScope.moduleStats.wrapper.readyState = vertxEventBus.readyState();
+        $rootScope.moduleStats.service.readyState = vertxEventBusService.readyState();
+        $rootScope.moduleStats.service.getConnectionState = vertxEventBusService.getConnectionState();
+        $rootScope.moduleStats.service.isEnabled = vertxEventBusService.isEnabled();
+        $rootScope.moduleStats.service.isConnected = vertxEventBusService.isConnected();
+      } catch (e) {}
+    }, 1000);
+  })
+  .filter('eventBusState', function () {
+    var states = ['CONNECTING', 'OPEN', 'CLOSING', 'CLOSED'];
+    return function (value) {
+      return states[value] || value;
+    };
   })
   .controller('MyController', function($scope, vertxEventBusService) {
     $scope.login = function (username, password) {
-      vertxEventBusService.login(username, password).then(function (reply) {
-        $scope.reply = reply;
-      })['catch'](function (reply) {
-        $scope.reply = reply;
-      });
+      vertxEventBusService.login(username, password)
+        .then(function (reply) {
+          $scope.reply = reply;
+        })
+        ['catch'](function (reply) {
+          $scope.reply = reply;
+        });
     };
 
     var holder = {};
@@ -37,6 +59,12 @@
       $scope.timeServiceActive = true;
     };
     $scope.deregisterTimeService = function () {
+      holder.timeServiceDeconstructor();
+      holder.timeServiceDeconstructor = undefined;
+      $scope.timeServiceActive = false;
+    };
+    $scope.deregisterTimeService2x = function () {
+      holder.timeServiceDeconstructor();
       holder.timeServiceDeconstructor();
       holder.timeServiceDeconstructor = undefined;
       $scope.timeServiceActive = false;
