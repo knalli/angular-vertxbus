6e0e88dd8212b98bd39e161fc063d8c3f9fa1228
knalli <knallisworld@googlemail.com>
2015-04-21 22:56:33 +0200

    Release 2.0.0-beta.1

--- dist/angular-vertxbus.js
+++ dist/angular-vertxbus.js
@@ -1,4 +1,4 @@
-/*! angular-vertxbus - v2.0.0-beta.1 - 2015-04-21
+/*! angular-vertxbus - v2.0.0-beta.2 - 2015-04-21
 * http://github.com/knalli/angular-vertxbus
 * Copyright (c) 2015 Jan Philipp; Licensed MIT */
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
