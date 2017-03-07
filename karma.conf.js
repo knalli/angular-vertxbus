ad7ab247b88ccf154574fbc5b77fdfad8035daeb
knalli <knallisworld@googlemail.com>
2017-03-07 23:33:26 +0100

    fix(tests): test_scopes should use scoped angular libs :bomb:

--- karma.conf.js
+++ karma.conf.js
@@ -144,8 +144,8 @@ module.exports = function (config) {
 
     // list of files / patterns to load in the browser
     files : [
-      'bower_components/angular/angular.js',
-      'bower_components/angular-mocks/angular-mocks.js',
+      injectByScope(scope, 'angular/angular.js'),
+      injectByScope(scope, 'angular-mocks/angular-mocks.js'),
       'node_modules/babel-polyfill/browser.js',
       'test/unit/test_index.js',
     ],
